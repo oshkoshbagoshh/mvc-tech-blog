d4dd9b6f70d07256070b09e61c7fb7899e4ad2a5
AJ Javadi <amirjavadi25@gmail.com>
2023-07-28 05:22:16 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mvc-tech-blog
+
+![Alt text](../../../NU-VIRT-FSF-FT-05-2023-U-LOLC/14-MVC/02-Challenge/Assets/14-mvc-homework-demo-01.gif)
---
+++ server.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: Someone 
+ * @Email: someone@example.com
+ * @Date: 2023-07-27 22:54:10 
+ * @Last Modified by:   undefined 
+ * @Last Modified time: 2023-07-27 22:54:10
+ * @Description: Set up the server
+ */
+
